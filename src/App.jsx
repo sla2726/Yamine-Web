1f370a92efe77ad57158573454d837eee2d37336
sla2726 <fabiodasilvajuniosilva@gmail.com>
2025-06-18 16:25:18 +0000

    ButtonTheme - dark/snow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <h1>Hello world!</h1>;
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  return (
+    <div className="min-h-screen transition-all bg-snow text-black dark:bg-slate-900 dark:text-white">
+      <div className="p-2">
+        <button
+          onClick={() => setDarkMode((prev) => !prev)}
+          className="py-2 px-4 bg-blue-300 text-white rounded-md"
+        >
+          Tema
+        </button>
+        <h1 className="mt-4 text-2xl">Hello world!</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        snow: "#F0F8FF",
+        darkBlue: "#1E293B",
+      },
+    },
   },
   plugins: [],
-}
-
+};
