ebe66e6ebf09593ad75a7e977446c65f40d5262e
sla2726 <fabiodasilvajuniosilva@gmail.com>
2025-06-19 13:37:45 +0000

    Animated Theme Button V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState, useEffect } from "react";
-import { AlignJustify } from "lucide-react";
+import { AlignJustify, Sun, Moon } from "lucide-react";
 
 function App() {
   // Light/Dark mode
   const [darkMode, setDarkMode] = useState(false);
+  const [isRotating, setIsRotating] = useState(false);
+
+  function toggleTheme() {
+    setIsRotating(true); // Rotação ativada
+    setTimeout(() => {
+      setDarkMode((prev) => !prev);
+      setIsRotating(false); // Rotação desativada
+    }, 300); // Tempo que vai girar
+  }
+
   useEffect(() => {
     if (darkMode) {
       document.documentElement.classList.add("dark");
@@ -19,10 +29,12 @@ function App() {
     <div className="h-screen w-screen transition-all bg-snow text-black dark:bg-slate-900 dark:text-white">
       <div className="p-2 bg-blue-300 relative">
         <button
-          onClick={() => setDarkMode((prev) => !prev)}
-          className="py-2 px-2 font-bold bg-darkBlue text-white dark:bg-snow dark:text-black rounded-md"
+          onClick={toggleTheme}
+          className={`py-2 px-2 font-bold bg-darkBlue text-white dark:bg-snow dark:text-black rounded-md transition-transform duration-300 ${
+            isRotating ? "rotate-180" : ""
+          }`}
         >
-          Tema
+          {darkMode ? <Moon /> : <Sun />}
         </button>
 
         <button
@@ -31,9 +43,13 @@ function App() {
         >
           <AlignJustify />
         </button>
-        {openMenu && (
-          <div className="fixed top-0 right-0 h-full w-32 z-10 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="bg-white dark:bg-slate-800 rounded-md w-3/4 max-w-sm">
+        <div
+          className={`fixed top-0 right-0 h-full w-48 z-10 p-4 bg-black bg-opacity-80 transition-transform duration-300 ${
+            openMenu ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          {openMenu && (
+            <div className="bg-white dark:bg-slate-800 rounded-md w-3/4 mt-10 ml-5">
               <h2 className="text-xl font-extrabold mb-4 flex justify-center">
                 Menu
               </h2>
@@ -41,8 +57,8 @@ function App() {
                 <li>Olokomeo</li>
               </ul>
             </div>
-          </div>
-        )}
+          )}
+        </div>
 
         <h1 className="text-2xl font-extrabold absolute inset-0 flex justify-center items-center pointer-events-none">
           YAMINE
