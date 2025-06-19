c578447544cb94b29872fc014140d1ea866eb489
sla2726 <fabiodasilvajuniosilva@gmail.com>
2025-06-19 22:50:07 +0000

    Memory allocation and New Texts

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,84 @@
 import { useState, useEffect } from "react";
-import { AlignJustify, Sun, Moon } from "lucide-react";
+import { AlignJustify, CircleX, BotMessageSquare } from "lucide-react";
 import ThemeAnimation from "./components/ThemeAnimation";
+import MenuItems from "./components/MenuItems";
 
 function App() {
   // Light/Dark mode
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    const saved = localStorage.getItem("theme");
+    return saved === "dark";
+  });
 
   useEffect(() => {
     if (darkMode) {
       document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
   }, [darkMode]);
 
   // Menu
   const [openMenu, setOpenMenu] = useState(false);
 
+  const addBotUrl =
+    "https://discord.com/oauth2/authorize?client_id=943330773916876820&scope=bot&permissions=8";
+
   return (
     <div className="h-screen w-screen transition-all bg-snow text-black dark:bg-slate-900 dark:text-white">
       <div className="p-2 bg-blue-300 relative">
+        {/* Textos principais */}
+        <h1 className="text-2xl font-raleway font-extrabold absolute inset-0 flex justify-center items-center pointer-events-none">
+          YAMINE
+        </h1>
+
+        {/* Button dark/light theme */}
         <button onClick={() => setDarkMode((prev) => !prev)}>
           <ThemeAnimation darkMode={darkMode} />
         </button>
 
+        {/* Button Menu */}
         <button
-          className="absolute top-2 right-4 py-2 px-2 z-20 rounded-md bg-darkBlue text-white dark:bg-snow dark:text-black"
+          className="absolute top-2 right-4 py-2 px-2 z-40 rounded-md bg-darkBlue text-white dark:bg-snow dark:text-black"
           onClick={() => setOpenMenu(!openMenu)}
         >
-          <AlignJustify />
+          {openMenu ? <CircleX /> : <AlignJustify />}
         </button>
+
+        {/* Menu */}
         <div
-          className={`fixed top-0 right-0 h-full w-48 z-10 p-4 bg-black bg-opacity-80 transition-transform duration-300 ${
+          className={`fixed top-0 right-0 h-full w-56 z-30 p-4 bg-black bg-opacity-80 transition-transform duration-300 ${
             openMenu ? "translate-x-0" : "translate-x-full"
           }`}
         >
-          {openMenu && (
-            <div className="bg-white dark:bg-slate-800 rounded-md w-3/4 mt-10 ml-5">
-              <h2 className="text-xl font-extrabold mb-4 flex justify-center">
-                Menu
-              </h2>
-              <ul className="space-y-2">
-                <li>Olokomeo</li>
-              </ul>
-            </div>
-          )}
-        </div>
+          <h2 className="text-xl font-extrabold mb-4 flex justify-center">
+            Menu
+          </h2>
 
-        <h1 className="text-2xl font-extrabold absolute inset-0 flex justify-center items-center pointer-events-none">
-          YAMINE
-        </h1>
+          <div className="bg-white dark:bg-slate-800 rounded-md w-3/4 mt-10 ml-5 p-2">
+            <MenuItems />
+          </div>
+        </div>
       </div>
+
+      <section className="p-6 mx-auto max-w-xl space-y-2 flex flex-col">
+        <h2 className="text-2xl font-bold font-nunito text-left w-fit self-start">
+          Apenas um bot do discord pronto para te ajudar!
+        </h2>
+        <a
+          href={addBotUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center justify-center text-2xl font-nunito bg-blue-400 p-2 rounded-md hover:underline hover:text-blue-700 transition-colors"
+        >
+          <span className="mr-2">
+            <BotMessageSquare />
+          </span>
+          Me adicione!
+        </a>
+      </section>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        caprasimo: ["Caprasimo", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+        oswald: ["Oswald", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+      },
       colors: {
         snow: "#F0F8FF",
         darkBlue: "#1E293B",
